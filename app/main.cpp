f8f67da30a96fecc0d4a52effbd17850a5d1879f
Itamar Hassin <ihassin@mac.com>
2018-03-04 01:04:35 +0000

    Ignoring logs

--- app/main.cpp
+++ app/main.cpp
@@ -4,6 +4,11 @@
 #include <termios.h>
 #include <stropts.h>
 
+#undef DEBUG
+#include <log4cpp/Category.hh>
+#include <log4cpp/PropertyConfigurator.hh>
+#define DEBUG
+
 #ifndef PIPERI_H
 #include "piperi.h"
 #endif
@@ -25,46 +30,50 @@ bool __alive = true;
 */
 int main(int argc, char **argv)
 {
-    std::cout << LIBBUNGET_VERSION_STRING << "\nTraceLevel: " << get_trace_level() << "\n";
+  log4cpp::PropertyConfigurator::configure("log4cpp.properties");
+  log4cpp::Category& rootLog = log4cpp::Category::getRoot();
+  log4cpp::Category& mainLog = log4cpp::Category::getInstance(std::string("main"));
+
+  if(argc==1) {
+    rootLog.error("sudo bunget hcidev#, pass device id as 0,1,2.. as first argument!");
+    return -1;
+  }
+
+  if(getuid()!=0) {
+    rootLog.error("run under sudo credentials or setcap bunget !\n");
+    return -1;
+  }
+
+  int dev = ::atoi(argv[1]);
+
+  int srdel = 0;
+  if(argc == 3) {
+    srdel = atoi(argv[2]);
+  }
+
+  enable_raw_mode();
+  
+  try {
+    BtCtx *ctx = BtCtx::instance();
+    Callback callback;
+
+    IServer *BS =  ctx->new_server(&callback, dev, "piperi", srdel, true);
+
+    IService *ps = BS->add_service(0x123F, "piperi");
+
+    callback.LedChr = ps->add_charact(UID_GPIO,PROPERTY_WRITE|PROPERTY_INDICATE,0, FORMAT_RAW, 1); // 1 / 0
+    callback.TimeChr = ps->add_charact(UID_TIME, PROPERTY_READ|PROPERTY_NOTIFY, 0, FORMAT_RAW, 20); // we send it as string
+    callback.Temp1Chr = ps->add_charact(UID_TEMP, PROPERTY_NOTIFY|PROPERTY_INDICATE, 0, FORMAT_FLOAT, FORMAT_FLOAT_LEN); // we send it as float
     
-    if(argc==1) {
-        std::cout << "sudo bunget hcidev#, pass device id as 0,1,2.. as first argument!\n";
-        return -1;
-    }
-    if(getuid()!=0) {
-        std::cout << "run under sudo credentials or setcap bunget !\n";
-        return -1;
-    }
+    BS->advertise(512);
+    mainLog.info("Advertising");
+    BS->run();
+    BS->stop();
+  } catch(bunget::hexecption& ex) {
+    mainLog.fatal(ex.report());
+  }
 
-    int dev = ::atoi(argv[1]);
-
-    int srdel = 0;
-    if(argc == 3) {
-        srdel = atoi(argv[2]);
-    }
-
-    enable_raw_mode();
-        
-    try {
-        BtCtx *ctx = BtCtx::instance();
-        Callback callback;
-
-        IServer *BS =  ctx->new_server(&callback, dev, "piperi", srdel, true);
-
-        IService *ps = BS->add_service(0x123F, "piperi");
-
-        callback.LedChr = ps->add_charact(UID_GPIO,PROPERTY_WRITE|PROPERTY_INDICATE,0, FORMAT_RAW, 1); // 1 / 0
-        callback.TimeChr = ps->add_charact(UID_TIME, PROPERTY_READ|PROPERTY_NOTIFY, 0, FORMAT_RAW, 20); // we send it as string
-        callback.Temp1Chr = ps->add_charact(UID_TEMP, PROPERTY_NOTIFY|PROPERTY_INDICATE, 0, FORMAT_FLOAT, FORMAT_FLOAT_LEN); // we send it as float
-
-        BS->advertise(512);
-        BS->run();
-        BS->stop();
-    } catch(bunget::hexecption& ex) {
-        ERROR (ex.report());
-    }
-
-    disable_raw_mode();
-    tcflush(0, TCIFLUSH);
-    return 0;
+  disable_raw_mode();
+  tcflush(0, TCIFLUSH);
+  return 0;
 }
